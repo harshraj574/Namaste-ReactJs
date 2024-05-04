59dd1948bf700404f47d7d776a8e8f4294bd224a
Harsh raj <harshraj574@gmail.com>
2024-05-04 23:47:21 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,38 +1,35 @@
-/*
-
-<div id ="parent">
-<div id = "child">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm a h2 tag</h2>
-</div>
-<div id="child2">
-    <h1>I'm a h1 tag</h1>
-    <h2>I'm a h2 tag</h2>
-</div>
-</div>
-
-*/
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},[
-    React.createElement(
-        "div",{id: "child"},
-        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"Ths is namaste React 🚀🚀")]
-        ),
-        React.createElement(
-            "div",{id: "child2"},
-            [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm a h2 tag")]
-            ) 
-    ]);
+//React.createElement =>React Element => JS Object => HTML element(render)
 
 
-// const heading = React.createElement("h1",{ id:"heading"},"Hello World from React!!");
+//JSX (transpiled before it reaches JS ENGINE) -> PARCEL - Babel
 
-// console.log(heading); //object
-console.log(parent);
+//JSX =>
+const jsxHeading  = <h1 className="head"> Namaste React using JSX 🚀🚀</h1>;
+console.log(jsxHeading);
+
+function Welcome(props){
+    return <h1>Welcome to react Component , {props.name} {props.surname}</h1>;
+}
+
+const element = <Welcome name="Harsh" surname="Raj" />;
+
+
+//A fucntional component
+function Wpp(){
+    return (
+        <div>
+            <Welcome name="Harsh" surname="Raj"/>
+            <Welcome name="Akash" surname="Gupta"/>
+            <Welcome name="Bassi" surname="Anubhav"/>
+        </div>
+    );
+}
+ 
+const element2 = <Wpp/>
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); //renders the heading react element to the root 
+root.render(element2);
